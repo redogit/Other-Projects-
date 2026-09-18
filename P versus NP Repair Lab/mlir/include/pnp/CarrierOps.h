f9754133a5606322793a5c9c81db09c2328602bb
redogit <69516108+redogit@users.noreply.github.com>
2026-09-18 01:38:22 -0400

    build: expose LLVM 23 bytecode interfaces to Carrier ops

--- P versus NP Repair Lab/mlir/include/pnp/CarrierOps.h
+++ P versus NP Repair Lab/mlir/include/pnp/CarrierOps.h
@@ -3,7 +3,9 @@
 
 #include "pnp/CarrierDialect.h"
 #include "pnp/CarrierTypes.h"
+#include "mlir/Bytecode/BytecodeOpInterface.h"
 #include "mlir/IR/OpDefinition.h"
+#include "mlir/IR/OpImplementation.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
 
 #define GET_OP_CLASSES
